36a71cb341c437dd00832b67bf2210722e906260
TanMosheng <92104087+TanMosheng@users.noreply.github.com>
2022-06-16 16:45:32 +0800

    Update 注册界面.php

--- HTML/注册界面.php
+++ HTML/注册界面.php
@@ -21,8 +21,8 @@
     }
 
     .btn {
-        background-color: skyblue;
-        background-image: linear-gradient(to right, rgb(241, 191, 147), rgb(247, 151, 72) 65%);
+        background-color: yellow;
+        background-image: linear-gradient(to right, rgb(238, 178, 113), rgb(252, 238, 117) 65%);
         border-radius: 5px;
         margin: 0px;
         border: 1px solid skyblue;
@@ -42,52 +42,115 @@
         transform: scale(0.9);
     }
 
+    .W {
+        text-align: center;
+        width: 600px;
+        height: 300px;
+        position: relative;
+        left: 35%;
+    }
+
+    .wrap {
+        width: 450px;
+        height: 250px;
+        position: relative;
+        top: 25%;
+
+    }
+
+    .item {
+        height: 30px;
+    }
+
+    label {
+        font-size: 16px;
+        width: 150px;
+        height: 2em;
+        display: inline-block;
+        text-align: right;
+        padding-right: 5px;
+        font-weight: bold;
+    }
+
+    .top {
+        width: 180px;
+    }
+
     .input {
         background-color: #fff;
         border: 1px solid white;
         padding: 0.9rem 0.9rem;
         margin: 0.5rem 0;
-        width: 10%;
+        width: 200px;
         border-radius: 20px;
     }
 
     /* 设置点击没有边框 */
-    input:focus {
+    .input:focus {
         outline: none;
         border: 1px solid orange;
     }
 </style>
 
 <body>
-    <br><br><br><br>
-    <strong>
-        <h1>用户注册</h1>
-    </strong>
-    <form action="http://localhost:63342/untitled/%E7%99%BB%E5%BD%95.html?_ijt=h2h10aoavo3j6cd9t8u550a30o" method="post">
-        学号/工号：
-        <input class="input" type="text" name="userNo" pattern="[0-9]" placeholder="请输入学号或工号" required="">
-        <br><br>
-        姓名：
-        <input class="input" type="text" name="name" placeholder="请输入姓名" required="">
-        <br><br>
-        性别：
-        <input type="radio" name="gender" value="M" checked="true"> 男 <input type="radio" name="gender" value="F"> 女
-        <br><br>
-        密码：
-        <input class="input" type="password" name="password" placeholder="请输入密码" required="">
-        <br><br>
-        再次输入密码：
-        <input class="input" type="password" name="password" placeholder="请确认密码" required="">
-        <br><br>
-        电话号码：
-        <input class="input" type="phone" name="phone" pattern="[0-9]{11}" placeholder="请输入11位电话号码" >
-        <br><br>
-        身份：
-        <input type="radio" name="userType" value="S" checked="true"> 学生 <input type="radio" name="userType" value="T"> 老师
-        <br><br>
-        <input class="btn" type="submit" value="提交">
-    </form>
-
+    <div class="W">
+        <br><br><br><br>
+        <strong>
+            <h1>用户注册</h1>
+        </strong>
+        <div class="wrap">
+            <form action="#" method="post">
+                <div class="item">
+                    <label>
+                        学工号：
+                    </label>
+                    <input class="input" type="text" name="userNo" pattern="[0-9]" placeholder="请输入学工号" required="">
+                </div>
+                <br><br>
+                <div class="item">
+                    <label>
+                        姓名：
+                    </label>
+                    <input class="input" type="text" name="name" placeholder="请输入姓名" required="">
+                </div>
+                <br><br>
+                <div class="item">
+                    <label>
+                        性别：
+                    </label>
+                    <input type="radio" name="gender" value="M" checked="true"> 男 <input type="radio" name="gender" value="F"> 女
+                </div>
+                <div class="item">
+                    <label>
+                        密码：
+                    </label>
+                    <input class="input" type="password" name="password" placeholder="请输入密码" required="">
+                </div>
+                <br><br>
+                <div class="item">
+                    <label>
+                        再次输入密码：
+                    </label>
+                    <input class="input" type="password" name="password" placeholder="请确认密码" required="">
+                </div>
+                <br><br>
+                <div class="item">
+                    <label>
+                        电话号码：
+                    </label>
+                    <input class="input" type="phone" name="phone" pattern="[0-9]{11}" placeholder="请输入11位电话号码">
+                </div>
+                <br><br>
+                <div class="item">
+                    <label>
+                        身份：
+                    </label>
+                    <input type="radio" name="userType" value="S" checked="true"> 学生 <input type="radio" name="userType" value="T"> 老师
+                </div>
+                <input class="btn" type="submit" value="提交">
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
